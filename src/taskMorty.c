ff0d3600978f191408b23fc0f31a7db5c9087e21
Franco Liza <fliza@dc.uba.ar>
2020-11-25 18:14:07 -0300

    subiendo tarea de test

--- src/taskMorty.c
+++ src/taskMorty.c
@@ -1,43 +1,33 @@
+#include "stddef.h"
 #include "syscall.h"
 #include "i386.h"
 
-void meeseks1_func(void);
+void meeseks2_func(void);
+void meeseks4_func(void);
 
 void task(void) {
-  syscall_meeseeks((uint32_t)&meeseks1_func, 40, 25);
+  syscall_meeseeks((uint32_t)&meeseks2_func, 75, 30);
+  syscall_meeseeks((uint32_t)&meeseks4_func, 70, 35);
 
   while (1) {
     __asm volatile("nop");
   }
 }
 
-void meeseks1_func(void) {
-  // Do Nothing!
+void meeseks2_func(void) {
+  for (int i = 0; i < 1000; i++) {
+    syscall_move(1, 0);
+  }
   while (1) {
     __asm volatile("nop");
   }
+}
 
-  int8_t deltax, deltay;
-
-  while (true) {
-    syscall_look(&deltax, &deltay);
-    syscall_move(deltax, deltay);
-    continue;
-    while (deltax < 0) {
-      syscall_move(-1, 0);
-      deltax++;
-    }
-    while (deltay < 0) {
-      syscall_move(0, -1);
-      deltay++;
-    }
-    while (deltax > 0) {
-      syscall_move(1, 0);
-      deltax--;
-    }
-    while (deltay > 0) {
-      syscall_move(0, 1);
-      deltay--;
-    }
+void meeseks4_func(void) {
+  for (int i = 0; i < 1000; i++) {
+    syscall_move(0, 1);
+  }
+  while (1) {
+    __asm volatile("nop");
   }
 }
--- src/taskRick.c
+++ src/taskRick.c
@@ -3,12 +3,11 @@
 #include "i386.h"
 
 void meeseks1_func(void);
-void meeseks2_func(void);
+void meeseks3_func(void);
 
 void task(void) {
-
-  syscall_meeseeks((uint32_t)&meeseks1_func, 21, 24);
-  syscall_meeseeks((uint32_t)&meeseks2_func, 6, 6);
+  syscall_meeseeks((uint32_t)&meeseks1_func, 3, 10);
+  syscall_meeseeks((uint32_t)&meeseks3_func, 10, 3);
 
   while (1) {
     __asm volatile("nop");
@@ -16,22 +15,19 @@ void task(void) {
 }
 
 void meeseks1_func(void) {
-  int8_t x;
-  int8_t y;
-  syscall_look(&x, &y);
+  for (int i = 0; i < 1000; i++) {
+    syscall_move(-1, 0);
+  }
   while (1) {
-    for (int i = 0; i < 80; i++) {
-      syscall_move(-1, 0);
-    }
-    syscall_move(0, -1);
+    __asm volatile("nop");
   }
 }
 
-void meeseks2_func(void) {
+void meeseks3_func(void) {
+  for (int i = 0; i < 1000; i++) {
+    syscall_move(0, -1);
+  }
   while (1) {
-    for (int i = 0; i < 80; i++) {
-      syscall_move(1, 0);
-    }
-    syscall_move(0, 1);
+    __asm volatile("nop");
   }
 }
